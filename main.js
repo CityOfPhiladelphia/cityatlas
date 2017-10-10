f6f4f25074cb944f5a2c2b1b474b2b03b259a58a
andy.rothwell <rothwell.andy@gmail.com>
2017-10-10 15:22:24 -0400

    changes baseconfig commit

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var GATEKEEPER_KEY = '82fe014b6575b8c38b44235580bc8b11';
 // var BASE_CONFIG_URL = '//raw.githubusercontent.com/rbrtmrtn/mapboard-base-config/develop/config.js';
-var BASE_CONFIG_URL = '//rawgit.com/rbrtmrtn/mapboard-base-config/b047f22e6cc59eaa6857008f04babe229ea6b00a/config.js';
+var BASE_CONFIG_URL = '//rawgit.com/rbrtmrtn/mapboard-base-config/8765aa641e561cdd7c9d374a96d2439378b49ab4/config.js';
 
 var ZONING_CODE_MAP = {
   'RSD-1': 'Residential Single Family Detached-1',
