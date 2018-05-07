67c16796ef11ace6cc2552d788b360432cec1bb9
andy.rothwell <rothwell.andy@gmail.com>
2018-05-07 17:54:56 -0400

    adding babel to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,11 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.js$/,
+        // exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader'],
       },
