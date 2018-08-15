d023999f0ca0caaff6917a7b90ed87e53a0e0160
andy.rothwell <rothwell.andy@gmail.com>
2018-08-15 16:03:42 -0400

    fixes dor condos problem

--- src/data-sources/dor-condo-list.js
+++ src/data-sources/dor-condo-list.js
@@ -13,8 +13,9 @@ export default {
   options: {
     params: {
       q: function(feature, state){
-        console.log(state.parcels.dor.data[0].properties, 'mapreg', state.parcels.dor.data[0].properties.MAPREG);
-        return "select * from condominium where mapref = '" + state.parcels.dor.data[0].properties.MAPREG + "'"
+        // console.log(state.parcels.dor.data[0].properties, 'mapreg', state.parcels.dor.data[0].properties.MAPREG);
+        return "select * from condominium where mapref = '" + state.parcels.dor.data[0].properties.MAPREG + "'\
+                                          and status in (1,3)"
       },// + "' or addresskey = '" + feature.properties.li_address_key.toString() + "'",
     }
   }
