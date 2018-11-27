98c77692c7c2c2c361e372a1f0a575fd20619325
andy.rothwell <rothwell.andy@gmail.com>
2018-11-27 14:51:27 -0500

    removes import of sentry from separate file

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ _________ .__  __            _____   __  .__
         \/          \/            \/               \/     \/
 */
 
-import './util/sentry_init';
 import * as Sentry from '@sentry/browser';
 Sentry.init({ dsn: 'https://bbd37729e48142faaefba93ff32e3c14@sentry.io/1331835' });
 
