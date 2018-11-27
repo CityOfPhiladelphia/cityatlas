c475552b2087cb9aa9bd25bfa21098730d15d78e
andy.rothwell <rothwell.andy@gmail.com>
2018-11-27 13:48:04 -0500

    fixes bug from sentry - https://sentry.io/city-of-philadelphia/atlas/issues/780619158/ - multiple polygons must return for 4413 Germantown Ave

--- src/main.js
+++ src/main.js
@@ -7,8 +7,9 @@ _________ .__  __            _____   __  .__
         \/          \/            \/               \/     \/
 */
 
-import * as Sentry from '@sentry/browser';
-Sentry.init({ dsn: 'https://de720599f539470782c0178091dcf6bf@sentry.io/1330846' });
+import './util/sentry_init';
+// import * as Sentry from '@sentry/browser';
+// Sentry.init({ dsn: 'https://bbd37729e48142faaefba93ff32e3c14@sentry.io/1331835' });
 
 import accounting from 'accounting';
 import axios from 'axios';
