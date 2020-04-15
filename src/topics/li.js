d190f63737c3c864afc4439fe22e6b17cc3616e5
Andy Rothwell <rothwell.andy@gmail.com>
2020-04-15 16:29:35 -0400

    finishes l&i schema changes

--- src/topics/li.js
+++ src/topics/li.js
@@ -249,7 +249,22 @@ export default {
           {
             label: 'ID',
             value: function(state, item){
-              return "<a target='_blank' href='http://li.phila.gov/#details?entity=violationdetails&eid="+item.casenumber+"&key="+item.addressobjectid+"&address="+encodeURIComponent(item.address)+"'>"+item.casenumber+" <i class='fa fa-external-link-alt'></i></a>";
+              var eclipseLocId = state.geocode.data.properties.eclipse_location_id.split('|');
+              var li_address_key = state.geocode.data.properties.li_address_key.split('|');
+              var j;
+              var str = '';
+              for (j = 0; j < li_address_key.length; j++) {
+                str += li_address_key[j];
+                str += ",";
+              }
+              var i;
+              for (i = 0; i < eclipseLocId.length; i++) {
+                str += eclipseLocId[i];
+                str += ",";
+              }
+              str = str.slice(0, str.length - 1);
+              // console.log('str:', str);
+              return "<a target='_blank' href='http://li.phila.gov/#details?entity=violationdetails&eid="+item.casenumber+"&key="+str+"&address="+encodeURIComponent(item.address)+"'>"+item.casenumber+" <i class='fa fa-external-link-alt'></i></a>";
             },
           },
           {
