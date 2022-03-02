1c1581b855e8a620bc3b12d8f150e8be2f1b9f88
Andy Rothwell <Andy.Rothwell@phila.gov>
2022-03-02 15:52:58 -0500

    uses mapboard 4.4.4 which fixes 311 topic

--- src/data-sources/311.js
+++ src/data-sources/311.js
@@ -1,10 +1,18 @@
 export default {
   id: 'threeOneOne',
   type: 'esri-nearby',
-  url: 'https://gis-int.databridge.phila.gov/arcgis/rest/services/Atlas/GIS311_365DAYS/MapServer/0',
+  url: 'https://services.arcgis.com/fLeGjb7u4uXqeF9q/ArcGIS/rest/services/SALESFORCE_CASES_1YEAR/FeatureServer/0',
   options: {
     geometryServerUrl: 'https://gis-utils.databridge.phila.gov/arcgis/rest/services/Utilities/Geometry/GeometryServer/',
     distances: 800,
     calculateDistance: true,
+    where: function() {
+      let today = new Date();
+      let dd = String(today.getDate()).padStart(2, '0');
+      let mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+      let yyyy = today.getFullYear() - 1;
+      let lastYear = yyyy + '-' + mm + '-' + dd;
+      return "REQUESTED_DATETIME > DATE '" + lastYear + "'";
+    },
   },
 };
