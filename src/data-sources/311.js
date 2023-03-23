e8f75f3e7b761b9abc461e2eea7689673137a8fe
Andy Rothwell <Andy.Rothwell@phila.gov>
2023-03-23 15:26:50 -0400

    uses @phila/mapboard 4.4.15, switches all geometry services to new service

--- src/data-sources/311.js
+++ src/data-sources/311.js
@@ -3,7 +3,7 @@ export default {
   type: 'esri-nearby',
   url: 'https://services.arcgis.com/fLeGjb7u4uXqeF9q/ArcGIS/rest/services/SALESFORCE_CASES_1YEAR/FeatureServer/0',
   options: {
-    geometryServerUrl: 'https://gis-utils.databridge.phila.gov/arcgis/rest/services/Utilities/Geometry/GeometryServer/',
+    geometryServerUrl: 'https://citygeo-geocoder-pub.databridge.phila.gov/arcgis/rest/services/Utilities/Geometry/GeometryServer/',
     distances: 800,
     calculateDistance: true,
     where: function() {
